c2726f1c85c20b627e0885a1a88890e29a0c3fb7
Tom Lim <t.y.lim-1@student.tudelft.nl>
2023-06-12 15:47:06 +0200

    visualize keyboard

--- improved_functions.py
+++ improved_functions.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 from permutationsga.problem import Solution
 
@@ -134,6 +136,38 @@ def insertion_mutation(s0: Solution, mutation_probability):
         assert len(s0.e) == len(mutated_layout), "The mutation caused a change in the solution size"
 
     return Solution(mutated_layout)
+
+def visualize_keyboard(solution):
+    # Rows based on QWERTY keyboard
+    row_indices = [[0, 9], [10, 18], [19, 25]]
+    
+    fig, ax = plt.subplots(figsize=(12, 3))
+    
+    # Loop through rows
+    for row_number, (row_start, row_end) in enumerate(row_indices):
+        for idx in range(row_start, row_end + 1):
+            key = solution[idx]
+            
+            # Compute x position
+            x = (idx - row_start) + 0.5 * row_number
+            
+            # Compute y position
+            y = -row_number
+            
+            # Draw the rectangle
+            width = 0.9
+            height = 0.9
+            rect = patches.Rectangle((x, y), width, height, linewidth=1, edgecolor='black', facecolor='lightgray')
+            ax.add_patch(rect)
+            
+            # Draw the character inside the rectangle
+            ax.text(x + width / 2, y + height / 2, key, ha='center', va='center', fontsize=12, fontweight='bold')
+    
+    ax.set_xlim(-1, 15)
+    ax.set_ylim(-3, 1)
+    ax.axis('off')
+    
+    plt.show()
 
 
 
--- test.py
+++ test.py
@@ -1,23 +1,37 @@
-import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
-# Define the keyboard layout as a list for easy indexing.
-keyboard_layout = list('QWERTYUIOPASDFGHJKLZXCVBNM')
+def visualize_keyboard(solution):
+    # Rows based on QWERTY keyboard
+    row_indices = [[0, 9], [10, 18], [19, 25]]
+    
+    fig, ax = plt.subplots(figsize=(12, 3))
+    
+    # Loop through rows
+    for row_number, (row_start, row_end) in enumerate(row_indices):
+        for idx in range(row_start, row_end + 1):
+            key = solution[idx]
+            
+            # Compute x position
+            x = (idx - row_start) + 0.5 * row_number
+            
+            # Compute y position
+            y = -row_number
+            
+            # Draw the rectangle
+            width = 0.9
+            height = 0.9
+            rect = patches.Rectangle((x, y), width, height, linewidth=1, edgecolor='black', facecolor='lightgray')
+            ax.add_patch(rect)
+            
+            # Draw the character inside the rectangle
+            ax.text(x + width / 2, y + height / 2, key, ha='center', va='center', fontsize=12, fontweight='bold')
+    
+    ax.set_xlim(-1, 15)
+    ax.set_ylim(-3, 1)
+    ax.axis('off')
+    
+    plt.show()
 
-# Define the sections.
-left_keys = list('QWERASDFZXCV')
-middle_keys = list('TYGHBN')
-right_keys = list('UIOPJKLM')
-
-# Use list comprehension to create the sections based on the indices in the keyboard layout.
-left_section = np.array([keyboard_layout.index(key) for key in left_keys])
-middle_section = np.array([keyboard_layout.index(key) for key in middle_keys])
-right_section = np.array([keyboard_layout.index(key) for key in right_keys])
-
-# Combine the sections into a list.
-keyboard_sections = np.array([left_section, middle_section, right_section]
-, dtype=object)
-
-section = np.random.choice(keyboard_sections)
-
-print('test')
-
+keyboard_layout = list("QWERTYUIOPASDFGHJKLZXCVBNM")
+visualize_keyboard(keyboard_layout)
